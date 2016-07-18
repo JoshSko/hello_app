e476c2e6ba4b35b689117f03056673bd4aa3749d
JoshSko <joshuaskosna1@gmail.com>
2016-07-18 14:39:03 +0200

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def world
-  	render html: "!goodbye,world!klakalkakalalal"
+  	render html: "!goodbye,world"
   end
 end
